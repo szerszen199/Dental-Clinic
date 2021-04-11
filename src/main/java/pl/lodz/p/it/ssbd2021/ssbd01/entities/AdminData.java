3d8b85e9acdd8962ae1675d88ba8832a2c2dccbe
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2021-04-12 00:30:43 +0200

    Dodanie klasy encyjnej dla poziomu dostepu recepcjonisty

---
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/entities/AdminData.java
@@ -0,0 +1,19 @@
+package pl.lodz.p.it.ssbd2021.ssbd01.entities;
+
+import java.io.Serializable;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+/**
+ * Typ Admin data reprezentujący poziom dostępu konta aplikacji dla admina.
+ */
+@Entity
+@DiscriminatorValue("level.admin")
+public class AdminData extends AccessLevel implements Serializable {
+
+    @Override
+    public String toString() {
+        return "pl.lodz.p.it.ssbd2021.ssbd01.entities.PatientData[ id=" + this.getId() + " ]";
+    }
+
+}
