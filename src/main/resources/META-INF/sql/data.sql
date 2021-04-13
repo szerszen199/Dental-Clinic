da79c9665f20ebcdd579366373fa4ff3332517bf
224466 Zdrzalik Przemysław <224466@edu.p.lodz.pl>
2021-04-13 17:27:07 +0200

    Dodano komentarze do plików .sql

--- src/main/resources/META-INF/sql/data.sql
+++ src/main/resources/META-INF/sql/data.sql
@@ -1,4 +1,9 @@
 
+-- Dodanie 4 użytkowników, oraz nadanie im poziomów dostępów.
+-- Użytkownik -1 ma reprezentować administratora oraz recepcjonistę będącego jednosczesnie pacjentem,
+-- -2 lekarza, -3 pacjenta a -4 recepcjonistę.
+-- Użykownik -4 posiada również odebrane uprawnienia do poziomu lekarza
+
 INSERT INTO accounts (id, email, password, first_name, last_name, language, version, enabled, created_by)
 VALUES (-1, 'jkowalski@mail.com', 'b03ddf3ca2e714a6548e7495e2a03f5e824eaac9837cd7f159c67b90fb4b7342', 'Jan', 'Kowalski',
         'pl', 0, true, -1),
