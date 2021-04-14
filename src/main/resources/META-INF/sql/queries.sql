b8b1531e70c2948702328a80ea109c14c284a7fd
224248 Antczak Piotr <224248@edu.p.lodz.pl>
2021-04-14 18:09:14 +0200

    dodano właściwość login, usunięto zbędne ograniczenie na level w tabeli access_levels

--- src/main/resources/META-INF/sql/queries.sql
+++ src/main/resources/META-INF/sql/queries.sql
@@ -1,7 +1,7 @@
 -- uwierzytelnienie
 SELECT *
 FROM accounts
-WHERE (email = 'jnowak@mail.com')
+WHERE (login = 'jnow123')
   AND (password = 'b03ddf3ca2e714a6548e7495e2a03f5e824eaac9837cd7f159c67b90fb4b7342')
   AND (active = 1)
   AND (enabled = 1);
@@ -9,10 +9,10 @@ WHERE (email = 'jnowak@mail.com')
 -- autoryzacja
 SELECT *
 FROM access_levels
-WHERE account_id = (SELECT id FROM accounts WHERE email = 'jnowak@mail.com')
+WHERE account_id = (SELECT id FROM accounts WHERE login = 'jnow123')
   AND (active = 1);
 -- poziomy dostępu
 SELECT *
 FROM access_levels
-WHERE account_id = (SELECT id FROM accounts WHERE email = 'jnowak@mail.com');
+WHERE account_id = (SELECT id FROM accounts WHERE login = 'jnow123');
 
