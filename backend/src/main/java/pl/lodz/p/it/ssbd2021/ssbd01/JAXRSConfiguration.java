f71aa789d1f690ef181deb72bd2c438188d90935
224466 Zdrzalik Przemysław <224466@edu.p.lodz.pl>
2021-04-18 18:53:16 +0200

    dodano plik z konfiguracją

---
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/JAXRSConfiguration.java
@@ -0,0 +1,9 @@
+package pl.lodz.p.it.ssbd2021.ssbd01;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class JAXRSConfiguration extends Application {
+
+}
