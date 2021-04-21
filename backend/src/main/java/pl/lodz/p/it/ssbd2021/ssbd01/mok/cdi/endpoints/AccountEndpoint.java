67cea3cededa20eeaab6b034a318a8a577bbe6ce
224383 Muszyński Grzegorz <224383@edu.p.lodz.pl>
2021-04-21 20:58:11 +0200

    Zaimplementowanie możliwości wyświetlenia danych o swoim koncie przez użytkownika

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mok/cdi/endpoints/AccountEndpoint.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mok/cdi/endpoints/AccountEndpoint.java
@@ -1,17 +1,21 @@
 package pl.lodz.p.it.ssbd2021.ssbd01.mok.cdi.endpoints;
 
 import java.text.ParseException;
+import javax.annotation.security.PermitAll;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.security.enterprise.SecurityContext;
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import pl.lodz.p.it.ssbd2021.ssbd01.entities.Account;
 import pl.lodz.p.it.ssbd2021.ssbd01.mok.ejb.managers.AccountManager;
 import pl.lodz.p.it.ssbd2021.ssbd01.security.JwtUtils;
-
 
 
 /**
@@ -47,4 +51,16 @@ public class AccountEndpoint {
         }
     }
 
+    /**
+     * Pobiera informacje o zalogowanm koncie.
+     *
+     * @return informacje o zalogowanym koncie
+     */
+    @GET
+    @Path("/info")
+    public Response getLoggedInAccountInfo() {
+            Account account = accountManager.getLoggedInAccount();
+            return Response.ok(account).build();
+    }
 }
+
