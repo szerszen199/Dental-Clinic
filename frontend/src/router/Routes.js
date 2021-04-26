9a07a99162d5d6b77815ddf99e13ffaad0319ad4
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2021-04-26 16:43:59 +0200

    Dodano obsluge niezaimplementowanych wyjatkow - error 404

--- frontend/src/router/Routes.js
+++ frontend/src/router/Routes.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "../containers/Home";
+import Home from "../containers/Home/Home";
+import Error404 from "../containers/Error404/Error404"
 
 export default function Routes() {
     return (
         <Switch>
             <Route exact path="/">
                 <Home />
+            </Route>
+            <Route>
+                <Error404 />
             </Route>
         </Switch>
     );
