6141d9044a488c76a90abb3c19186b575827e0cf
224466 Zdrzalik Przemysław <224466@edu.p.lodz.pl>
2021-04-26 17:24:47 +0200

    Dodano stronę logowania, narazie nie polaczana z backendem

--- frontend/src/router/Routes.js
+++ frontend/src/router/Routes.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "../containers/Home/Home";
 import Error404 from "../containers/Error404/Error404"
+import Registration from "../containers/Registration/Registration";
 
 export default function Routes() {
     return (
         <Switch>
             <Route exact path="/">
                 <Home />
+            </Route>
+            <Route exact path="/register">
+                <Registration/>
             </Route>
             <Route>
                 <Error404 />
