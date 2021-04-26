88a6939927acca99fbdbdd79f57ad5a253fddf27
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2021-04-26 17:12:12 +0200

    Dodano strone logowania

--- frontend/src/router/Routes.js
+++ frontend/src/router/Routes.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "../containers/Home/Home";
-import Error404 from "../containers/Error404/Error404"
+import Error404 from "../containers/Error404/Error404";
+import Login from "../containers/Login/Login";
 
 export default function Routes() {
     return (
         <Switch>
             <Route exact path="/">
                 <Home />
+            </Route>
+            <Route exact path="/login">
+                <Login />
             </Route>
             <Route>
                 <Error404 />
