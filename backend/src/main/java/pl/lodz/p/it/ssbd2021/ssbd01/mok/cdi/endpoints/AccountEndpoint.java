a0716a658b069fe570958c0f9bf89faaf49bb69c
224406 Witold Pietrzak <224406@edu.p.lodz.pl>
2021-04-29 04:29:18 +0200

    "Usunięto koniecznosc podawania id w endpoincie i logice biznesowej"

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mok/cdi/endpoints/AccountEndpoint.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mok/cdi/endpoints/AccountEndpoint.java
@@ -1,40 +1,31 @@
 package pl.lodz.p.it.ssbd2021.ssbd01.mok.cdi.endpoints;
 
-import java.text.ParseException;
-import java.util.List;
-import java.util.stream.Collectors;
-import javax.annotation.security.PermitAll;
+import pl.lodz.p.it.ssbd2021.ssbd01.entities.PatientData;
+import pl.lodz.p.it.ssbd2021.ssbd01.exceptions.AccessLevelException;
+import pl.lodz.p.it.ssbd2021.ssbd01.exceptions.BaseException;
+import pl.lodz.p.it.ssbd2021.ssbd01.mok.dto.AccessLevelDto;
+import pl.lodz.p.it.ssbd2021.ssbd01.mok.dto.AccountDto;
+import pl.lodz.p.it.ssbd2021.ssbd01.mok.ejb.managers.AccessLevelManager;
+import pl.lodz.p.it.ssbd2021.ssbd01.mok.ejb.managers.AccountManager;
+import pl.lodz.p.it.ssbd2021.ssbd01.security.JwtUtils;
+import pl.lodz.p.it.ssbd2021.ssbd01.utils.converters.AccessLevelConverter;
+import pl.lodz.p.it.ssbd2021.ssbd01.utils.converters.AccountConverter;
+
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.security.enterprise.SecurityContext;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import pl.lodz.p.it.ssbd2021.ssbd01.entities.Account;
-import pl.lodz.p.it.ssbd2021.ssbd01.mok.dto.AccountDto;
-
-import javax.ws.rs.core.Response;
-
-import pl.lodz.p.it.ssbd2021.ssbd01.entities.PatientData;
-import pl.lodz.p.it.ssbd2021.ssbd01.mok.dto.AccessLevelDto;
-import pl.lodz.p.it.ssbd2021.ssbd01.mok.dto.AccountDto;
-
-import pl.lodz.p.it.ssbd2021.ssbd01.mok.ejb.managers.AccessLevelManager;
-import pl.lodz.p.it.ssbd2021.ssbd01.exceptions.BaseException;
-
-import pl.lodz.p.it.ssbd2021.ssbd01.exceptions.AccessLevelException;
-import pl.lodz.p.it.ssbd2021.ssbd01.mok.ejb.managers.AccountManager;
-import pl.lodz.p.it.ssbd2021.ssbd01.security.JwtUtils;
-import pl.lodz.p.it.ssbd2021.ssbd01.utils.converters.AccessLevelConverter;
-import pl.lodz.p.it.ssbd2021.ssbd01.utils.converters.AccountConverter;
+import java.text.ParseException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -90,15 +81,14 @@ public class AccountEndpoint {
     /**
      * Edit account data.
      *
-     * @param id         Id of edited account.
      * @param accountDto Account with edited data.
      * @throws BaseException Base exception.
      */
     @POST
-    @Path("edit/{id}")
+    @Path("edit")
     @Produces({MediaType.APPLICATION_JSON})
-    public void editAccount(@PathParam("id") Long id, AccountDto accountDto) throws BaseException {
-        accountManager.editAccount(id, AccountConverter.createAccountEntityFromDto(accountDto));
+    public void editAccount(AccountDto accountDto) throws BaseException {
+        accountManager.editAccount(AccountConverter.createAccountEntityFromDto(accountDto));
     }
 
 
@@ -190,7 +180,7 @@ public class AccountEndpoint {
     }
 
     /**
-     * Pobiera listę wszystkich kont
+     * Pobiera listę wszystkich kont.
      *
      * @return lista wszystkich kont
      */
