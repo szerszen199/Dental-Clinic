ae59dbb34f65625a9b429b927b2196bac5269ea6
224406 Witold Pietrzak <224406@edu.p.lodz.pl>
2021-05-06 00:45:16 +0200

    "Stworzono interfejs użytkownika dla przypadku użycia 'Wyswietl listę kont' #SSBD202101-174 "

--- frontend/src/router/Routes.js
+++ frontend/src/router/Routes.js
@@ -4,6 +4,7 @@ import Home from "../containers/Home/Home";
 import Error404 from "../containers/Error404/Error404"
 import Registration from "../containers/Registration/Registration";
 import Login from "../containers/Login/Login";
+import Accounts from "../containers/AccountsList/Accounts";
 
 export default function Routes() {
     return (
@@ -16,6 +17,10 @@ export default function Routes() {
             </Route>
             <Route exact path="/login">
                 <Login />
+            </Route>
+
+            <Route exact path="/accounts">
+                <Accounts/>
             </Route>
             <Route>
                 <Error404 />
