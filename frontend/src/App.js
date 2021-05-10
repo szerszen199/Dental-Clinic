29bf31d9f140abb83415c27226daa60444e5dfd7
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2021-05-11 00:12:16 +0200

    Dodano naglowek dla zalogowanego uzytkownika - SSBD202101-190

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,35 +3,67 @@ import Navbar from "react-bootstrap/Navbar";
 import "./App.css";
 import Routes from "./router/Routes";
 import Nav from "react-bootstrap/Nav";
-import { LinkContainer } from "react-router-bootstrap";
+import {LinkContainer} from "react-router-bootstrap";
 
 function App() {
-    return (
-        <div className="App container py-3">
-            <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-                <LinkContainer to="/">
-                    <Navbar.Brand className="font-weight-bold text-muted">
-                        Home
-                    </Navbar.Brand>
-                </LinkContainer>
-                <Navbar.Toggle />
-                <Navbar.Collapse className="justify-content-end">
-                    <Nav activeKey={window.location.pathname}>
-                        <LinkContainer to="/register">
-                            <Nav.Link>Register</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/login">
-                            <Nav.Link>Login</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/accounts">
-                            <Nav.Link>Accounts</Nav.Link>
-                        </LinkContainer>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-            <Routes />
-        </div>
-    );
+    if (false === true) {
+        return (
+            <div className="App container py-3">
+                <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+                    <LinkContainer to="/">
+                        <Navbar.Brand className="font-weight-bold text-muted">
+                            Home
+                        </Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle/>
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav activeKey={window.location.pathname}>
+                            <LinkContainer to="/register">
+                                <Nav.Link>Register</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <Nav.Link>Login</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/accounts">
+                                <Nav.Link>Accounts</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                <Routes/>
+            </div>
+        );
+    } else {
+        return (
+            <div className="App container py-3">
+                <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+                    <LinkContainer to="/">
+                        <Navbar.Brand className="font-weight-bold text-muted">
+                            Home
+                        </Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle/>
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav activeKey={window.location.pathname}>
+                            <LinkContainer to="/appointments">
+                                <Nav.Link>Appointments</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/prescriptions">
+                                <Nav.Link>Prescriptions</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/account">
+                                <Nav.Link>Account</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/accounts">
+                                <Nav.Link>Accounts</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                <Routes/>
+            </div>
+        );
+    }
 }
 
 export default App;
