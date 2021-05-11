51164f268eec59a5b57e41e2bc25e6d0cf560098
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2021-05-11 11:28:53 +0200

    Rozdzielenie widoku konta na edycje konta i konto, dodanie opcji ustawienia ciemnego motywu w ustawieniach konta - SSBD202101-190

--- frontend/src/components/Account/Account.js
+++ frontend/src/components/Account/Account.js
@@ -1,167 +1,26 @@
 import React from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 import "./Account.css";
+import EditAccount from "../EditAccount/EditAccount";
+import {Col, Container, ListGroup, Row} from "react-bootstrap";
 
-export default class Account extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isDisabled: true,
-            text: "Edit",
-            login: "",
-            email:"",
-            password: "",
-            firstName: "",
-            lastName: "",
-            phoneNumber : "",
-            pesel: ""
-        }
-    }
-
-    validateForm() {
-        // Todo: zrobić walidację taką jaką wymaga projekt
-        function loginCorrect() {
-            return this.state.login.length > 0;
-        }
-
-        function emailCorrect() {
-            return this.state.email.length > 0;
-        }
-
-        function passwordCorrect() {
-            return this.state.password.length > 0;
-        }
-
-        function firstNameCorrect() {
-            return this.state.firstName.length > 0;
-        }
-
-        function lastNameCorrect() {
-            return this.state.lastName.length > 0;
-        }
-
-        function phoneNumberCorrect() {
-            return this.state.phoneNumber.length > 0;
-        }
-
-        // TODO: przypadek obcokrajowca wymusza że peselu może nie być ale nadal warto by go zwalidowac, tylko jak?
-        function peselCorrect() {
-            return true;
-        }
-
-        return emailCorrect() && passwordCorrect() && loginCorrect() && firstNameCorrect() && lastNameCorrect() && phoneNumberCorrect() && peselCorrect();
-    }
-
-
-    // Todo: prawdopodobnie wysyłać zapytanie do backendu tutaj, chciałbym zrobić tak jak w vue się da żeby jeśli odpalam w trybie debug front to łącze z localhostem, narazie nie ruszam.
-    handleSubmit(event) {
-        event.preventDefault();
-    }
-
-    handleOnClick() {
-        if(this.state.isDisabled === true){
-            this.setEditable()
-        } else{
-            this.setNotEditable()
-        }
-    }
-
-    setEditable(){
-        this.setState({
-            isDisabled: false,
-            text: "Save"
-        });
-    }
-
-    setNotEditable(){
-        this.validateForm()
-        this.setState({
-            isDisabled: true,
-            text: "Edit"
-        });
-    }
-
-    // todo: Czy dodawać tutaj też język do wyboru z en / pl? W dto go nie ma
-    render() {
-
-        return (
-            <div className="Registration">
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group size="lg" controlId="login">
-                        <Form.Label>Login</Form.Label>
-                        <Form.Control
-                            autoFocus
-                            type="login"
-                            value={this.state.login}
-                            disabled={true}
-                            onChange={(e) => this.setState({login: e.target.value})}
-                        />
-                    </Form.Group>
-                    <Form.Group size="lg" controlId="email">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control
-                            autoFocus
-                            type="email"
-                            value={this.state.email}
-                            disabled={this.state.isDisabled}
-                            onChange={(e) => this.setState({email: e.target.value})}
-                        />
-                    </Form.Group>
-                    <Form.Group size="lg" controlId="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                            type="password"
-                            value={this.state.password}
-                            disabled={this.state.isDisabled}
-                            onChange={(e) => this.setState({password: e.target.value})}
-                        />
-                    </Form.Group>
-                    <Form.Group size="lg" controlId="firstName">
-                        <Form.Label>First Name</Form.Label>
-                        <Form.Control
-                            type="text"
-                            value={this.state.firstName}
-                            disabled={this.state.isDisabled}
-                            onChange={(e) => this.setState({pesel: e.target.value})}
-                        />
-                    </Form.Group>
-                    <Form.Group size="lg" controlId="lastName">
-                        <Form.Label>Last Name</Form.Label>
-                        <Form.Control
-                            type="text"
-                            value={this.state.lastName}
-                            disabled={this.state.isDisabled}
-                            onChange={(e) => this.setState({lastName: e.target.value})}
-                        />
-                    </Form.Group>
-                    <Form.Group size="lg" controlId="phoneNumber">
-                        <Form.Label>Phone Number</Form.Label>
-                        <Form.Control
-                            type="text"
-                            value={this.state.phoneNumber}
-                            disabled={this.state.isDisabled}
-                            onChange={(e) => this.setState({phoneNumber: e.target.value})}
-                        />
-                    </Form.Group>
-                    {/*Todo: co z peselem dla obcokrajowca? Nic czy coś innnego? Narazie zrobiłem że może być pusty*/}
-                    <Form.Group size="lg" controlId="pesel">
-                        <Form.Label>Pesel</Form.Label>
-                        <Form.Control
-                            type="text"
-                            value={this.state.pesel}
-                            disabled={this.state.isDisabled}
-                            onChange={(e) => this.setState({pesel: e.target.value})}
-                        />
-                    </Form.Group>
-                    <Button block size="lg" type="submit"
-                        // disabled={!validateForm()}
-                            onClick={this.handleOnClick.bind(this)}>
-                        {this.state.text}
-                    </Button>
-                </Form>
-            </div>
-        );
-    }
+function Account() {
+    return (
+        <div className="EditAccount">
+            {/*<ListGroup horizontal>*/}
+            {/*    <ListGroup.Item><EditAccount/></ListGroup.Item>*/}
+            {/*    <ListGroup.Item>ListGroup</ListGroup.Item>*/}
+            {/*    <ListGroup.Item>renders</ListGroup.Item>*/}
+            {/*    <ListGroup.Item>horizontally!</ListGroup.Item>*/}
+            {/*</ListGroup>*/}
+            <Container>
+                <Row>
+                    <Col>2 of 2</Col>
+                    <Col><EditAccount/></Col>
+                    <Col/>
+                </Row>
+            </Container>
+        </div>
+    );
 }
+
+export default Account;
---
+++ frontend/src/components/EditAccount/EditAccount.js
@@ -0,0 +1,187 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./EditAccount.css";
+import Switch from "react-switch";
+
+export default class EditAccount extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isDisabled: true,
+            text: "Edit",
+            login: "",
+            email: "",
+            password: "",
+            repeatedPassword: "",
+            firstName: "",
+            lastName: "",
+            phoneNumber: "",
+            pesel: "",
+            darkMode: false,
+        }
+    }
+
+    validateForm() {
+        // Todo: zrobić walidację taką jaką wymaga projekt
+        function loginCorrect() {
+            return this.state.login.length > 0;
+        }
+
+        function emailCorrect() {
+            return this.state.email.length > 0;
+        }
+
+        function passwordCorrect() {
+            return this.state.password.length > 0;
+        }
+
+        function firstNameCorrect() {
+            return this.state.firstName.length > 0;
+        }
+
+        function lastNameCorrect() {
+            return this.state.lastName.length > 0;
+        }
+
+        function phoneNumberCorrect() {
+            return this.state.phoneNumber.length > 0;
+        }
+
+        // TODO: przypadek obcokrajowca wymusza że peselu może nie być ale nadal warto by go zwalidowac, tylko jak?
+        function peselCorrect() {
+            return true;
+        }
+
+        return emailCorrect() && passwordCorrect() && loginCorrect() && firstNameCorrect() && lastNameCorrect() && phoneNumberCorrect() && peselCorrect();
+    }
+
+
+    // Todo: prawdopodobnie wysyłać zapytanie do backendu tutaj, chciałbym zrobić tak jak w vue się da żeby jeśli odpalam w trybie debug front to łącze z localhostem, narazie nie ruszam.
+    handleSubmit(event) {
+        event.preventDefault();
+    }
+
+    handleOnClick() {
+        if (this.state.isDisabled === true) {
+            this.setEditable()
+        } else {
+            this.setNotEditable()
+        }
+    }
+
+    setEditable() {
+        this.setState({
+            isDisabled: false,
+            text: "Save"
+        });
+    }
+
+    setNotEditable() {
+        this.validateForm()
+        this.setState({
+            isDisabled: true,
+            text: "Edit"
+        });
+    }
+
+    // todo: Czy dodawać tutaj też język do wyboru z en / pl? W dto go nie ma
+    render() {
+        return (
+            <div className="Registration">
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group size="lg" controlId="login">
+                        <Form.Label>Login</Form.Label>
+                        <Form.Control
+                            autoFocus
+                            type="login"
+                            value={this.state.login}
+                            disabled={true}
+                            onChange={(e) => this.setState({login: e.target.value})}
+                        />
+                    </Form.Group>
+                    <Form.Group size="lg" controlId="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                            autoFocus
+                            type="email"
+                            value={this.state.email}
+                            disabled={this.state.isDisabled}
+                            onChange={(e) => this.setState({email: e.target.value})}
+                        />
+                    </Form.Group>
+                    {/*<Form.Group size="lg" controlId="password">*/}
+                    {/*    <Form.Label>Password</Form.Label>*/}
+                    {/*    <Form.Control*/}
+                    {/*        type="password"*/}
+                    {/*        value={this.state.password}*/}
+                    {/*        disabled={this.state.isDisabled}*/}
+                    {/*        onChange={(e) => this.setState({password: e.target.value})}*/}
+                    {/*    />*/}
+                    {/*</Form.Group>*/}
+                    {/*<Form.Group size="lg" controlId="password">*/}
+                    {/*    <Form.Label> Repeat password</Form.Label>*/}
+                    {/*    <Form.Control*/}
+                    {/*        type="password"*/}
+                    {/*        value={this.state.repeatedPassword}*/}
+                    {/*        disabled={this.state.isDisabled}*/}
+                    {/*        onChange={(e) => this.setState({password: e.target.value})}*/}
+                    {/*    />*/}
+                    {/*</Form.Group>*/}
+                    <Form.Group size="lg" controlId="firstName">
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={this.state.firstName}
+                            disabled={this.state.isDisabled}
+                            onChange={(e) => this.setState({pesel: e.target.value})}
+                        />
+                    </Form.Group>
+                    <Form.Group size="lg" controlId="lastName">
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={this.state.lastName}
+                            disabled={this.state.isDisabled}
+                            onChange={(e) => this.setState({lastName: e.target.value})}
+                        />
+                    </Form.Group>
+                    <Form.Group size="lg" controlId="phoneNumber">
+                        <Form.Label>Phone Number</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={this.state.phoneNumber}
+                            disabled={this.state.isDisabled}
+                            onChange={(e) => this.setState({phoneNumber: e.target.value})}
+                        />
+                    </Form.Group>
+                    {/*Todo: co z peselem dla obcokrajowca? Nic czy coś innnego? Narazie zrobiłem że może być pusty*/}
+                    <Form.Group size="lg" controlId="pesel">
+                        <Form.Label>Pesel</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={this.state.pesel}
+                            disabled={this.state.isDisabled}
+                            onChange={(e) => this.setState({pesel: e.target.value})}
+                        />
+                    </Form.Group>
+                    <Form.Group style={{marginTop: "20px"}}>
+                        <Form.Label style={{marginRight: "10px", transform: "translateY(-30%)"}}>Dark mode</Form.Label>
+                        <Switch
+                            onChange={(checked: boolean) => {
+                                this.setState({ darkMode: checked })
+                            }}
+                            disabled={this.state.isDisabled}
+                            checked={this.state.darkMode} />
+                    </Form.Group>
+                    <Button block size="lg" type="submit"
+                        // disabled={!validateForm()}
+                            onClick={this.handleOnClick.bind(this)}>
+                        {this.state.text}
+                    </Button>
+                </Form>
+            </div>
+        );
+    }
+}
