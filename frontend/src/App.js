148ba528bf728769148a911d1cd41dbaa8291166
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2021-05-11 11:48:42 +0200

    Rozdzielono aplikację na widoki pacjent i gosc - SSBD202101-190

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,67 +1,16 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
 import "./App.css";
-import Routes from "./router/Routes";
-import Nav from "react-bootstrap/Nav";
-import {LinkContainer} from "react-router-bootstrap";
+import Patient from "./views/Patient/Patient";
+import Guest from "./views/Guest/Guest";
 
 function App() {
     if (false === true) {
         return (
-            <div className="App container py-3">
-                <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-                    <LinkContainer to="/">
-                        <Navbar.Brand className="font-weight-bold text-muted">
-                            Home
-                        </Navbar.Brand>
-                    </LinkContainer>
-                    <Navbar.Toggle/>
-                    <Navbar.Collapse className="justify-content-end">
-                        <Nav activeKey={window.location.pathname}>
-                            <LinkContainer to="/register">
-                                <Nav.Link>Register</Nav.Link>
-                            </LinkContainer>
-                            <LinkContainer to="/login">
-                                <Nav.Link>Login</Nav.Link>
-                            </LinkContainer>
-                            <LinkContainer to="/accounts">
-                                <Nav.Link>Accounts</Nav.Link>
-                            </LinkContainer>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-                <Routes/>
-            </div>
+            <Guest/>
         );
     } else {
         return (
-            <div className="App container py-3">
-                <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-                    <LinkContainer to="/dashboard">
-                        <Navbar.Brand className="font-weight-bold text-muted">
-                            Home
-                        </Navbar.Brand>
-                    </LinkContainer>
-                    <Navbar.Toggle/>
-                    <Navbar.Collapse className="justify-content-end">
-                        <Nav activeKey={window.location.pathname}>
-                            <LinkContainer to="/appointments">
-                                <Nav.Link>Appointments</Nav.Link>
-                            </LinkContainer>
-                            <LinkContainer to="/prescriptions">
-                                <Nav.Link>Prescriptions</Nav.Link>
-                            </LinkContainer>
-                            <LinkContainer to="/account">
-                                <Nav.Link>Account</Nav.Link>
-                            </LinkContainer>
-                            <LinkContainer to="/accounts">
-                                <Nav.Link>Accounts</Nav.Link>
-                            </LinkContainer>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-                <Routes/>
-            </div>
+            <Patient/>
         );
     }
 }
---
+++ frontend/src/views/Patient/Patient.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import "./Patient.css";
+import Routes from "../../router/Routes";
+import Nav from "react-bootstrap/Nav";
+import {LinkContainer} from "react-router-bootstrap";
+
+function Patient() {
+    return (
+        <div className="App container py-3">
+            <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+                <LinkContainer to="/dashboard">
+                    <Navbar.Brand className="font-weight-bold text-muted">
+                        Home
+                    </Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Toggle/>
+                <Navbar.Collapse className="justify-content-end">
+                    <Nav activeKey={window.location.pathname}>
+                        <LinkContainer to="/appointments">
+                            <Nav.Link>Appointments</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/prescriptions">
+                            <Nav.Link>Prescriptions</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/account">
+                            <Nav.Link>Account</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/accounts">
+                            <Nav.Link>Accounts</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <Routes/>
+        </div>
+    );
+}
+
+export default Patient;
