1a7d1b774086302b813e949d60fb7051028d9e57
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2021-05-11 11:48:42 +0200

    Rozdzielono aplikację na widoki pacjent i gosc - SSBD202101-190

--- frontend/src/components/Account/Account.js
+++ frontend/src/components/Account/Account.js
@@ -1,17 +1,11 @@
 import React from "react";
 import "./Account.css";
 import EditAccount from "../EditAccount/EditAccount";
-import {Col, Container, ListGroup, Row} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 
 function Account() {
     return (
         <div className="EditAccount">
-            {/*<ListGroup horizontal>*/}
-            {/*    <ListGroup.Item><EditAccount/></ListGroup.Item>*/}
-            {/*    <ListGroup.Item>ListGroup</ListGroup.Item>*/}
-            {/*    <ListGroup.Item>renders</ListGroup.Item>*/}
-            {/*    <ListGroup.Item>horizontally!</ListGroup.Item>*/}
-            {/*</ListGroup>*/}
             <Container>
                 <Row>
                     <Col>2 of 2</Col>
---
+++ frontend/src/views/Guest/Guest.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import "./Guest.css";
+import Routes from "../../router/Routes";
+import Nav from "react-bootstrap/Nav";
+import {LinkContainer} from "react-router-bootstrap";
+
+function Guest() {
+    return (
+        <div className="App container py-3">
+            <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+                <LinkContainer to="/">
+                    <Navbar.Brand className="font-weight-bold text-muted">
+                        Home
+                    </Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Toggle/>
+                <Navbar.Collapse className="justify-content-end">
+                    <Nav activeKey={window.location.pathname}>
+                        <LinkContainer to="/register">
+                            <Nav.Link>Register</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/login">
+                            <Nav.Link>Login</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <Routes/>
+        </div>
+    );
+}
+
+export default Guest;
