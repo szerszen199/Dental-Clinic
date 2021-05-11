e2e991c56c9ef9a319dd723df251dbbfb1ad6249
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2021-05-11 12:33:52 +0200

    Dodanie formularzu zmiany hasla oraz poprawienie zapisu edcji konta i hasla - SSBD202101-190

--- frontend/src/PrivateRoute.js
+++ frontend/src/PrivateRoute.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import {Route, Redirect} from 'react-router-dom';
-// import {isLogin} from '../utils';
 
-// const PrivateRoute = ({component: Component, ...rest}) => {
-//     return (
-//         <Route {...rest} render={props => (
-//             isLogin() ?
-//                 <Component {...props} />
-//                 : <Redirect to="/login" />
-//         )} />
-//     );
-// };
 
 const PrivateRoute = ({component: Component, authed, ...rest}) => {
     return (
