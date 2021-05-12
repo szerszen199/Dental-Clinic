56cb4efc549badce625564a22a410d62dfac0785
224459 Woźniak Tomasz <224459@edu.p.lodz.pl>
2021-05-13 00:51:42 +0200

    Dodano DTO dla opowiedzi przy logowaniu z informacjami o użytkowniku, które jest zwracane razem z tokenem JWT #SSBD202101-203

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mok/dto/response/JwtResponseDTO.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mok/dto/response/JwtResponseDTO.java
@@ -9,18 +9,20 @@ public class JwtResponseDTO {
     private final String username;
     private final Set<String> roles;
     private final String token;
+    private final UserInfoResponseDTO userInfoResponseDTO;
 
     /**
      * Tworzy nową instancję klasy.
-     *
-     * @param username username
+     *  @param username username
      * @param roles    roles
      * @param token    token
+     * @param userInfoResponseDTO informacje o użytkowniku
      */
-    public JwtResponseDTO(String username, Set<String> roles, String token) {
+    public JwtResponseDTO(String username, Set<String> roles, String token, UserInfoResponseDTO userInfoResponseDTO) {
         this.username = username;
         this.roles = roles;
         this.token = token;
+        this.userInfoResponseDTO = userInfoResponseDTO;
     }
 
     /**
