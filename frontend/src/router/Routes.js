ce82860b1b75ba289c70471fdfb0f9bf1e32414c
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2021-05-16 22:50:38 +0200

    Poprawienie routingu - SSBD202101-257

--- frontend/src/router/Routes.js
+++ frontend/src/router/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 import Home from "../components/Home/Home";
 import Error404 from "../components/Error404/Error404"
 import Registration from "../components/Registration/Registration";
@@ -12,20 +12,26 @@ import Prescription from "../components/Prescription/Prescription"
 import MyAppointment from "../components/Appointment/MyAppointment/MyAppointment";
 import PlanAppointment from "../components/Appointment/PlanAppointment/PlanAppointment";
 import ListDoctors from "../components/Appointment/ListDoctors/ListDoctors";
+import HomeRoute from "../HomeRoute";
 
 export default function Routes() {
     return (
         <Switch>
             <Route exact path="/">
-                <Home />
+                <Redirect to="/home" />
             </Route>
+            <HomeRoute authed={true} path='/home' component={Dashboard} />
             <Route exact path="/register">
                 <Registration/>
             </Route>
+            <Route exact path="/home">
+                <Home/>
+            </Route>
+
             <Route exact path="/login">
                 <Login />
             </Route>
-            <PrivateRoute authed={true} path='/dashboard' component={Dashboard} />
+            {/*<PrivateRoute authed={true} path='/dashboard' component={Dashboard} />*/}
             <PrivateRoute authed={true} path='/prescriptions' component={Prescription} />
             <PrivateRoute authed={true} path='/account' component={Account} />
             <PrivateRoute authed={true} path='/accounts' component={Accounts} />
@@ -35,6 +41,7 @@ export default function Routes() {
             <Route>
                 <Error404 />
             </Route>
+
         </Switch>
     );
 }
