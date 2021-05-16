53e4c4c164005a08a1f49b43880679d3367767b2
224466 Zdrzalik Przemysław <224466@edu.p.lodz.pl>
2021-05-16 22:10:41 +0200

    dodano breadcrumbs #SSBD202101-191

--- frontend/src/views/Patient/Patient.js
+++ frontend/src/views/Patient/Patient.js
@@ -5,9 +5,9 @@ import Routes from "../../router/Routes";
 import Nav from "react-bootstrap/Nav";
 import {LinkContainer} from "react-router-bootstrap";
 import {DarkModeSwitch} from 'react-toggle-dark-mode';
-import {NavDropdown} from "react-bootstrap";
+import {Col, Container, NavDropdown, Row} from "react-bootstrap";
 import "react-custom-flag-select/lib/react-custom-flag-select.min.css";
-
+import BreadCrumbs from "../../components/Breadcrumbs/Breadcrumbs";
 
 function Patient() {
     const [isDarkMode, setIsDarkMode] = useState(() => false);
@@ -37,57 +37,50 @@ function Patient() {
     return (
         <div className="App container py-3 ">
             <Navbar collapseOnSelect bg="light" expand="md" className="shadow-box-example mb-3">
-                <LinkContainer to="/dashboard">
-                    <Navbar.Brand className="font-weight-bold text-muted">
-                        Home
-                    </Navbar.Brand>
-                </LinkContainer>
-                <Navbar.Toggle/>
-                <Navbar.Collapse className="justify-content-end">
-                    <Nav activeKey={window.location.pathname}>
-                        <NavDropdown title="Appointments" id="navbarScrollingDropdown">
-                            <NavDropdown.Item>
-                                <LinkContainer to="/my-appointments">
-                                    <Nav.Link>My appointments</Nav.Link>
+                <div style={{width: "100%"}}>
+                    <Container fluid>
+                        <Row>
+                            <Col>
+                                <LinkContainer to="/dashboard">
+                                    <Navbar.Brand className="font-weight-bold text-muted">
+                                        Home
+                                    </Navbar.Brand>
                                 </LinkContainer>
-                            </NavDropdown.Item>
-                            <NavDropdown.Item>
-                                <LinkContainer to="/plan-appointment">
-                                    <Nav.Link>Plan an appointment</Nav.Link>
-                                </LinkContainer>
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider/>
-                            <NavDropdown.Item>
-                                <LinkContainer to="/list-doctors">
-                                    <Nav.Link>List of doctors</Nav.Link>
-                                </LinkContainer>
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                        <LinkContainer to="/prescriptions">
-                            <Nav.Link>Prescriptions</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/account">
-                            <Nav.Link>Account</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/accounts">
-                            <Nav.Link>Accounts</Nav.Link>
-                        </LinkContainer>
-                    </Nav>
-                    <DarkModeSwitch
-                        style={{marginLeft: '1rem'}}
-                        checked={isDarkMode}
-                        onChange={setIsDarkMode}
-                        size={30}
-                        sunColor={"#FFDF37"}
-                        moonColor={"#bfbfbb"}
-                    />
-                    <img onClick={handleOnClick} style={{marginLeft: "10px", maxWidth:"30px"}} src={flag} alt="Logo" />
 
-                </Navbar.Collapse>
+
+                            </Col>
+                            <Col>
+                                <Navbar.Toggle/>
+                                <Navbar.Collapse className="justify-content-end">
+                                    <Nav activeKey={window.location.pathname}>
+                                        <LinkContainer to="/account">
+                                            <Nav.Link>My Account</Nav.Link>
+                                        </LinkContainer>
+                                        <LinkContainer to="/accounts">
+                                            <Nav.Link>User's Accounts</Nav.Link>
+                                        </LinkContainer>
+                                    </Nav>
+                                    <DarkModeSwitch
+                                        style={{marginLeft: '1rem'}}
+                                        checked={isDarkMode}
+                                        onChange={setIsDarkMode}
+                                        size={30}
+                                        sunColor={"#FFDF37"}
+                                        moonColor={"#bfbfbb"}
+                                    />
+                                    <img onClick={handleOnClick} style={{marginLeft: "10px", maxWidth: "30px"}}
+                                         src={flag} alt="Logo"/>
+                                </Navbar.Collapse>
+                            </Col>
+                        </Row>
+                        <Row> <Col> <BreadCrumbs/> </Col></Row>
+                    </Container>
+                </div>
             </Navbar>
             <Routes/>
         </div>
     );
 }
+
 
 export default Patient;
