9a8eae047f3be1375090d0858585ca0e1e9c7daa
224466 Zdrzalik Przemysław <224466@edu.p.lodz.pl>
2021-05-16 23:19:13 +0200

    Polaczono klasy do widoku w jedną #SSBD202101-260

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,11 @@
 import React from "react";
 import "./App.css";
-import Patient from "./views/Patient/Patient";
-import Guest from "./views/Guest/Guest";
-import Admin from "./views/Admin/Admin";
-import Receptionist from "./views/Receptionist/Receptionist";
-import Doctor from "./views/Doctor/Doctor";
+import Wszystko from "./views/Wszystko/Wszystko";
 
 function App() {
-    if (false === true) {
-        return (
-            <Guest/>
-        );
-    } else {
-        return (
-            // <Patient/>
-            // <Admin/>
-            // <Receptionist/>
-            <Doctor/>
-        );
-    }
+    return (
+        <Wszystko/>);
+
 }
 
 export default App;
--- frontend/src/views/Guest/Guest.js
+++ frontend/src/views/Guest/Guest.js
@@ -1,46 +1,19 @@
-import React, {useState} from "react";
-import Navbar from "react-bootstrap/Navbar";
+import React from "react";
 import "./Guest.css";
-import { useLocation } from "react-router-dom";
-import Routes from "../../router/Routes";
 import Nav from "react-bootstrap/Nav";
 import {LinkContainer} from "react-router-bootstrap";
-import {DarkModeSwitch} from 'react-toggle-dark-mode';
-import BreadCrumbs from "../../components/Breadcrumbs/Breadcrumbs";
 
 function Guest() {
-    const [isDarkMode, setIsDarkMode] = useState(() => false);
     return (
-        <div className="App container py-3">
-            <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-                <LinkContainer to="/">
-                    <Navbar.Brand className="font-weight-bold text-muted">
-                        Home
-                    </Navbar.Brand>
-                </LinkContainer>
-                <Navbar.Toggle/>
-                <Navbar.Collapse className="justify-content-end">
-                    <Nav activeKey={window.location.pathname}>
-                        <LinkContainer to="/register">
-                            <Nav.Link>Register</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/login">
-                            <Nav.Link>Login</Nav.Link>
-                        </LinkContainer>
-                    </Nav>
-                    <DarkModeSwitch
-                        style={{marginLeft: '1rem'}}
-                        checked={isDarkMode}
-                        onChange={setIsDarkMode}
-                        size={30}
-                        sunColor={"#FFDF37"}
-                        moonColor={"#bfbfbb"}
-                    />
-                </Navbar.Collapse>
-            </Navbar>
-            <BreadCrumbs/>
-            <Routes/>
-        </div>
+        <Nav activeKey={window.location.pathname}>
+            <LinkContainer to="/register">
+                <Nav.Link>Register</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+                <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+        </Nav>
+
     );
 }
 
