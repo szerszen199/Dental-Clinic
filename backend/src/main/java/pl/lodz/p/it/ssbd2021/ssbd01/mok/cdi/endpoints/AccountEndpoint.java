0e3c9bc1b89d3256d819c2ddfefc46bc96d5acf8
224466 Zdrzalik Przemysław <224466@edu.p.lodz.pl>
2021-05-17 22:17:04 +0200

    dodano zmienne srodowiskowe do projeku react #SSBD202101-261

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mok/cdi/endpoints/AccountEndpoint.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mok/cdi/endpoints/AccountEndpoint.java
@@ -236,7 +236,6 @@ public class AccountEndpoint {
      * @throws AppBaseException wyjątek typu AppBaseException
      */
     @GET
-    @RolesAllowed({RolesStringsTmp.admin})
     @Produces({MediaType.APPLICATION_JSON})
     @Path("/accounts")
     public Response getAllAccounts() throws AppBaseException {
