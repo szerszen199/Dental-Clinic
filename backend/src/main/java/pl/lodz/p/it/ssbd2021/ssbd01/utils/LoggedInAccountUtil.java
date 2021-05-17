8a40b072bb32574e22752c316f1d6e49e8af8563
224406 Witold Pietrzak <224406@edu.p.lodz.pl>
2021-05-18 00:44:06 +0200

    Naprawiono dzialanie LoggedInAccountUtil #SSBD202101-209

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/utils/LoggedInAccountUtil.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/utils/LoggedInAccountUtil.java
@@ -1,12 +1,13 @@
 package pl.lodz.p.it.ssbd2021.ssbd01.utils;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.security.enterprise.SecurityContext;
 import javax.ws.rs.core.Context;
 
 @Stateless
 public class LoggedInAccountUtil {
-    @Context
+    @Inject
     private SecurityContext securityContext;
 
     public String getLoggedInAccountLogin() {
