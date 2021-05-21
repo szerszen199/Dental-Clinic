9cb36acf811665cd66f8a1f81a0e6c3a4ad85046
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2021-05-21 18:10:23 +0200

    Dodanie tluamczen dla widoku goscia - #SSBD202101-270

--- frontend/src/views/Guest/Guest.js
+++ frontend/src/views/Guest/Guest.js
@@ -2,19 +2,21 @@ import React from "react";
 import "./Guest.css";
 import Nav from "react-bootstrap/Nav";
 import {LinkContainer} from "react-router-bootstrap";
+import {useTranslation} from "react-i18next";
 
-function Guest() {
+const Guest = () => {
+    const {t} = useTranslation();
+
     return (
         <Nav activeKey={window.location.pathname}>
             <LinkContainer to="/register">
-                <Nav.Link>Register</Nav.Link>
+                <Nav.Link>{t("Register")}</Nav.Link>
             </LinkContainer>
             <LinkContainer to="/login">
-                <Nav.Link>Login</Nav.Link>
+                <Nav.Link>{t("Login")}</Nav.Link>
             </LinkContainer>
         </Nav>
-
     );
-}
+};
 
 export default Guest;
