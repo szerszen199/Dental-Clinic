8a1e4f0cad853cd1f30a718c9944a293b855e49a
224406 Witold Pietrzak <224406@edu.p.lodz.pl>
2021-05-21 20:37:00 +0200

    Dostosowano kod do nowego sposobu przechowywania tokenu JWT #202101-243

--- frontend/src/components/AccountsList/AccountsListRequest.js
+++ frontend/src/components/AccountsList/AccountsListRequest.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import {Account} from "./Account";
+import Cookies from "js-cookie";
+import {JWTTokenCookieName} from "../Login/LoginRequest";
 
 // TODO usuwanie tego gdy minie określony czas czytaj przedawni się
 
@@ -9,13 +11,14 @@ export const userRolesStorageName = 'userRoles'
 
 export async function makeAccountsListRequest() {
 
+    let token = Cookies.get(JWTTokenCookieName);
     let recivedData = await axios.get(process.env.REACT_APP_BACKEND_URL + "account/accounts", {
-        headers: {"Authorization": "Bearer " + localStorage.getItem(JWTTokenStorageName)}
+        headers: {"Authorization": "Bearer " + token}
     })
     let data = recivedData.data
     let accounts = []
     for (const i in recivedData.data) {
-        accounts.push(new Account(data[i].firstName + " " + data[i].lastName, data[i].email,data[i].login))
+        accounts.push(new Account(data[i].firstName + " " + data[i].lastName, data[i].email, data[i].login))
     }
     return accounts
 }
