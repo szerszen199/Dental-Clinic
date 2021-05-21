1bd43c0bb4e5d645c359a73c31a3cbfdb51229e2
224248 Antczak Piotr <224248@edu.p.lodz.pl>
2021-05-21 19:40:39 +0200

    dodano brakującą adnotację @NotNull w DarkModeDto #SSBD202101-313

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mok/dto/DarkModeDto.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mok/dto/DarkModeDto.java
@@ -1,10 +1,13 @@
 package pl.lodz.p.it.ssbd2021.ssbd01.mok.dto;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * Klasa DarkModeDto dla motywu ciemnego.
  */
 public class DarkModeDto {
 
+    @NotNull
     private boolean isDarkMode;
 
     public boolean isDarkMode() {
@@ -13,6 +16,7 @@ public class DarkModeDto {
 
     /**
      * Tworzy nową instancję klasy DarkModeDto.
+     *
      * @param isDarkMode ustawienie trybu ciemnego
      */
     public DarkModeDto(boolean isDarkMode) {
