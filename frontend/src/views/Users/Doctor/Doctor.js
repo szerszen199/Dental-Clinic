1b4c49e535b960d0835d3a52f94b8c94ef41fff4
224406 Witold Pietrzak <224406@edu.p.lodz.pl>
2021-05-22 23:02:05 +0200

    Naprawiono dzialanie linkow #SSBD202101-268

--- frontend/src/views/Users/Doctor/Doctor.js
+++ frontend/src/views/Users/Doctor/Doctor.js
@@ -5,7 +5,7 @@ import {NavDropdown} from "react-bootstrap";
 import "react-custom-flag-select/lib/react-custom-flag-select.min.css";
 import MyAccount from "../Common/Common";
 import {useTranslation} from "react-i18next";
-
+import {Link} from "react-router-dom";
 
 function Doctor() {
     const {t} = useTranslation();
@@ -13,14 +13,14 @@ function Doctor() {
     return (
         <Nav activeKey={window.location.pathname}>
             <NavDropdown title={t("Appointments")} id="navbarScrollingDropdown">
-                <Nav.Link style={{color: "rgb(127, 127, 127)"}} className="navMenu"
+                <Nav.Link as={Link} style={{color: "rgb(127, 127, 127)"}} className="navMenu"
                           to="/my-appointments">{t("My appointments")}</Nav.Link>
                 <NavDropdown.Divider/>
-                <Nav.Link style={{color: "rgb(127, 127, 127)"}} className="navMenu"
+                <Nav.Link as={Link} style={{color: "rgb(127, 127, 127)"}} className="navMenu"
                           to="/list-doctors">{t("List of doctors")}</Nav.Link>
             </NavDropdown>
-            <Nav.Link to="/prescriptions">{t("Prescriptions")}</Nav.Link>
-            <Nav.Link to="/dashboard">{t("Documentation")}</Nav.Link>
+            <Nav.Link as={Link} to="/prescriptions">{t("Prescriptions")}</Nav.Link>
+            <Nav.Link as={Link} to="/dashboard">{t("Documentation")}</Nav.Link>
             <MyAccount/>
         </Nav>
     );
