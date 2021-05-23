59335f9ef14ac47c7c5909e0953e292ae32b7dd9
224406 Witold Pietrzak <224406@edu.p.lodz.pl>
2021-05-23 16:52:48 +0200

    Zmieniono typ uzywanej tabeli w AccountsList, dodano internacjonalizacje tabeli, dodano mozliwosc wyszukiwania w tabeli #SSBD202101-226

--- frontend/src/components/AccountsList/AccountsList.js
+++ frontend/src/components/AccountsList/AccountsList.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {Suspense} from "react";
 import "./AccountsList.css";
-import {Table} from "react-bootstrap";
 import {makeAccountsListRequest} from "./AccountsListRequest";
+import {withTranslation} from "react-i18next";
+import BootstrapTable from 'react-bootstrap-table-next';
+import {Dropdown, Table} from "react-bootstrap";
+import filterFactory, {textFilter} from 'react-bootstrap-table2-filter';
 
-class AccountsList extends React.Component {
+class AccountsListWithoutTranslation extends React.Component {
 
 
     constructor(props) {
@@ -22,7 +25,7 @@ class AccountsList extends React.Component {
     renderAccount(person, index) {
         return (
             <tr key={index}>
-                <td>{index+1}</td>
+                <td>{index + 1}</td>
                 <td>{person.login}</td>
                 <td>{person.name}</td>
                 <td>{person.email}</td>
@@ -31,18 +34,51 @@ class AccountsList extends React.Component {
     }
 
     renderAccounts() {
+        const {t} = this.props;
+
+        const columns = [
+
+            {
+                dataField: 'login',
+                text: t('Login-Noun'),
+                filter: textFilter({
+                    placeholder: t("Filter"),
+                    style: {marginLeft: "20px"}
+                })
+            },
+            {
+                dataField: 'name',
+                text: t('Name and Surname'),
+                filter: textFilter({
+                    placeholder: t("Filter"),
+                    style: {marginLeft: "20px"}
+                })
+            },
+            {
+                dataField: 'email',
+                text: t('Email'),
+                filter: textFilter({
+                    placeholder: t("Filter"),
+                    style: {marginLeft: "20px"}
+                })
+            }
+        ]
+        return <BootstrapTable striped keyField='id' columns={columns} data={this.state.accountsList} filter={filterFactory()}/>;
         return <Table striped bordered hover>
             <thead>
             <tr>
                 <th>#</th>
                 <th>
-                    <div className="LoginInfo">Login</div>
+                    <div className="LoginInfo">{t("Login-Noun")}</div>
+
                 </th>
                 <th>
-                    <div className="NameInfo">Name and surname</div>
+
+                    <div className="NameInfo">{t("Name and Surname")}</div>
+
                 </th>
                 <th>
-                    <div className="EmailInfo">Email</div>
+                    <div className="EmailInfo">{t("Email")}</div>
                 </th>
             </tr>
             </thead>
@@ -64,5 +100,13 @@ class AccountsList extends React.Component {
     }
 }
 
-export default AccountsList
 
+const AccountsListTr = withTranslation()(AccountsListWithoutTranslation)
+
+export default function AccountsList() {
+    return (
+        <Suspense fallback="loading">
+            <AccountsListTr/>
+        </Suspense>
+    );
+}
