42dd2dcd94bce2a23919716aa0b36a1e5be9ac42
224406 Witold Pietrzak <224406@edu.p.lodz.pl>
2021-05-23 04:59:54 +0200

    Dodano usuwanie pliku cookie z aktualna rola podczas wylogowywania  #202101-268

--- frontend/src/components/Login/Logout.js
+++ frontend/src/components/Login/Logout.js
@@ -4,6 +4,7 @@ import Cookies from 'js-cookie'
 export function logout() {
     Cookies.remove(process.env.REACT_APP_JWT_TOKEN_COOKIE_NAME);
     Cookies.remove(process.env.REACT_APP_ROLES_COOKIE_NAME);
+    Cookies.remove(process.env.REACT_APP_ACTIVE_ROLE_COOKIE_NAME);
     localStorage.setItem(process.env.REACT_APP_JWT_REFRESH_TOKEN_STORAGE_NAME, null);
     // TODO: To redirect po wylogowaniu zalogowaniu, nie podoba mi się, nie korzysta z routera ale inaczej mi nie chce narazie pojsc.
     window.location = "/login";
