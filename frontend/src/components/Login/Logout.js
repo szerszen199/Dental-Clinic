2224bf3c2e2a9939b4fc4862cc086deeac2519db
224248 Antczak Piotr <224248@edu.p.lodz.pl>
2021-05-24 15:31:38 +0200

    poprawiono pobieranie loginu po zmianie DTO w Backendzie po niedokładnie złączonym PR

--- frontend/src/components/Login/Logout.js
+++ frontend/src/components/Login/Logout.js
@@ -5,6 +5,7 @@ export function logout() {
     Cookies.remove(process.env.REACT_APP_JWT_TOKEN_COOKIE_NAME);
     Cookies.remove(process.env.REACT_APP_ROLES_COOKIE_NAME);
     Cookies.remove(process.env.REACT_APP_ACTIVE_ROLE_COOKIE_NAME);
+    Cookies.remove(process.env.REACT_APP_LOGIN_COOKIE)
     localStorage.setItem(process.env.REACT_APP_JWT_REFRESH_TOKEN_STORAGE_NAME, null);
     // TODO: To redirect po wylogowaniu zalogowaniu, nie podoba mi się, nie korzysta z routera ale inaczej mi nie chce narazie pojsc.
     window.location = "/login";
