0bfe638967cce200b0f76fa6408b2ba053968772
224248 Antczak Piotr <224248@edu.p.lodz.pl>
2021-05-24 22:28:39 +0200

    dodano warunek logiczny na cookie w MainView.js #SSBD202101-227

--- frontend/src/components/Login/LoginRequest.js
+++ frontend/src/components/Login/LoginRequest.js
@@ -12,9 +12,9 @@ export function makeLoginRequest(login, password) {
         // TODO: Czas expieracji.
         Cookies.set(process.env.REACT_APP_JWT_TOKEN_COOKIE_NAME, response.data.authJwtToken.token, {expires: process.env.jwtCookieExpirationTime});
         Cookies.set(process.env.REACT_APP_ROLES_COOKIE_NAME, response.data.roles, {expires: process.env.jwtCookieExpirationTime});
-        Cookies.set(process.env.REACT_APP_LOGIN_COOKIE, response.data.username,{expires: process.env.jwtCookieExpirationTime});
-        Cookies.set(process.env.REACT_APP_DARK_MODE_COOKIE, response.data.username,{expires: process.env.jwtCookieExpirationTime})
-        if(Cookies.get(process.env.REACT_APP_ACTIVE_ROLE_COOKIE_NAME) == null) {
+        Cookies.set(process.env.REACT_APP_LOGIN_COOKIE, response.data.username, {expires: process.env.jwtCookieExpirationTime});
+        Cookies.set(process.env.REACT_APP_DARK_MODE_COOKIE, response.data.isDarkMode, {expires: process.env.jwtCookieExpirationTime})
+        if (Cookies.get(process.env.REACT_APP_ACTIVE_ROLE_COOKIE_NAME) == null) {
             Cookies.set(process.env.REACT_APP_ACTIVE_ROLE_COOKIE_NAME, findDefaultRole(response.data.roles), {expires: process.env.jwtCookieExpirationTime});
         }
         localStorage.setItem(process.env.REACT_APP_JWT_REFRESH_TOKEN_STORAGE_NAME, response.data.refreshJwtToken.token);
