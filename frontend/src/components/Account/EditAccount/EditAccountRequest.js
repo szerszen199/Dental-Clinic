16cc2d4eb8df17fcc2ba538b32328378c0004c63
224248 Antczak Piotr <224248@edu.p.lodz.pl>
2021-05-24 15:07:36 +0200

    poprawiono wysyłanie żądań przez axios #SSBD202101-224

--- frontend/src/components/Account/EditAccount/EditAccountRequest.js
+++ frontend/src/components/Account/EditAccount/EditAccountRequest.js
@@ -1,21 +1,23 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-export function editAccountRequest(email, firstName, lastName, phoneNumber, pesel){
+export function editAccountRequest(email, firstName, lastName, phoneNumber, pesel,version, etag){
     let token = Cookies.get(process.env.REACT_APP_JWT_TOKEN_COOKIE_NAME);
     let config = {
         method: 'post',
         url: process.env.REACT_APP_BACKEND_URL + "account/edit",
         headers: {
             'Authorization': 'Bearer ' + token,
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            "If-Match": etag
         },
         data: {
             email: email,
             firstName: firstName,
             lastName: lastName,
             phoneNumber: phoneNumber,
-            pesel: pesel
+            pesel: pesel,
+            version: version
         }
     };
 
