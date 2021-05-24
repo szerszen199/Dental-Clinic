f1583f7cf178858f282fc9c9f6ef6b165a677129
224248 Antczak Piotr <224248@edu.p.lodz.pl>
2021-05-24 12:22:08 +0200

    poprawiono CorsFilter.java, dodano JavaDoc #SSBS202101-224

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mok/dto/common/AccessLevelWithActiveDto.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mok/dto/common/AccessLevelWithActiveDto.java
@@ -4,6 +4,12 @@ public abstract class AccessLevelWithActiveDto extends AccessLevelDto {
 
     private boolean isActive;
 
+    /**
+     * Koonstruktor dla klasy AccessLevelWithActiveDto.
+     *
+     * @param level poziom dostępu
+     * @param isActive status aktywności
+     */
     public AccessLevelWithActiveDto(String level, boolean isActive) {
         super(level);
         this.isActive = isActive;
