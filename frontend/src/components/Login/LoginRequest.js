4a208346fcae1ed610764df48e4e39c750c2c33a
224466 Zdrzalik Przemysław <224466@edu.p.lodz.pl>
2021-05-25 21:06:33 +0200

    fix cookies #SSBD202101-337

--- frontend/src/components/Login/LoginRequest.js
+++ frontend/src/components/Login/LoginRequest.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import Cookies from 'js-cookie'
 import findDefaultRole from "../../roles/findDefaultRole";
+import {jwtCookieExpirationTime} from "../../views/MainView/MainView";
 
 // TODO usuwanie tego gdy minie określony czas czytaj przedawni się
 
@@ -10,12 +11,12 @@ export function makeLoginRequest(login, password) {
         password: password
     }).then((response) => {
         // TODO: Czas expieracji.
-        Cookies.set(process.env.REACT_APP_JWT_TOKEN_COOKIE_NAME, response.data.authJwtToken.token, {expires: process.env.jwtCookieExpirationTime});
-        Cookies.set(process.env.REACT_APP_ROLES_COOKIE_NAME, response.data.roles, {expires: process.env.jwtCookieExpirationTime});
-        Cookies.set(process.env.REACT_APP_LOGIN_COOKIE, response.data.username,{expires: process.env.jwtCookieExpirationTime})
-        Cookies.set(process.env.REACT_APP_LANGUAGE_COOKIE, response.data.userInfoResponseDTO.language, {expires: process.env.jwtCookieExpirationTime})
+        Cookies.set(process.env.REACT_APP_JWT_TOKEN_COOKIE_NAME, response.data.authJwtToken.token, {expires: jwtCookieExpirationTime});
+        Cookies.set(process.env.REACT_APP_ROLES_COOKIE_NAME, response.data.roles, {expires: jwtCookieExpirationTime});
+        Cookies.set(process.env.REACT_APP_LOGIN_COOKIE, response.data.username,{expires: jwtCookieExpirationTime})
+        Cookies.set(process.env.REACT_APP_LANGUAGE_COOKIE, response.data.userInfoResponseDTO.language, {expires: jwtCookieExpirationTime})
         if(Cookies.get(process.env.REACT_APP_ACTIVE_ROLE_COOKIE_NAME) == null) {
-            Cookies.set(process.env.REACT_APP_ACTIVE_ROLE_COOKIE_NAME, findDefaultRole(response.data.roles), {expires: process.env.jwtCookieExpirationTime});
+            Cookies.set(process.env.REACT_APP_ACTIVE_ROLE_COOKIE_NAME, findDefaultRole(response.data.roles), {expires: jwtCookieExpirationTime});
         }
         localStorage.setItem(process.env.REACT_APP_JWT_REFRESH_TOKEN_STORAGE_NAME, response.data.refreshJwtToken.token);
         // TODO: To redirect po poprawnym zalogowaniu, nie podoba mi się, nie korzysta z routera ale inaczej mi nie chce narazie pojsc.
