c826bff442189ab3aafe3189d86514ee3c23bbee
224459 Woźniak Tomasz <224459@edu.p.lodz.pl>
2021-05-25 23:27:33 +0200

    Podpięto zmianę języka frontendu z backendem, dodatkowo zmieniono sposób ustawiania darkmode, aby był zgodny z językiem #SSBD202101-328

---
+++ frontend/src/components/Language/LanguageRequest.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+export function languageRequest(language){
+    let token = Cookies.get(process.env.REACT_APP_JWT_TOKEN_COOKIE_NAME);
+    let config = {
+        method: 'put',
+        url: process.env.REACT_APP_BACKEND_URL + "account/language",
+        headers: {
+            'Authorization': 'Bearer ' + token,
+            'Content-Type': 'application/json',
+        },
+        data: {
+            language: language
+        }
+    };
+
+    axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
--- frontend/src/components/Login/LoginRequest.js
+++ frontend/src/components/Login/LoginRequest.js
@@ -15,7 +15,7 @@ export function makeLoginRequest(login, password) {
         Cookies.set(process.env.REACT_APP_ROLES_COOKIE_NAME, response.data.roles, {expires: jwtCookieExpirationTime});
         Cookies.set(process.env.REACT_APP_LOGIN_COOKIE, response.data.username,{expires: jwtCookieExpirationTime})
         Cookies.set(process.env.REACT_APP_LANGUAGE_COOKIE, response.data.userInfoResponseDTO.language, {expires: jwtCookieExpirationTime})
-        Cookies.set(process.env.REACT_APP_DARK_MODE_COOKIE, response.data.userInfoResponseDTO.darkMode, {expires: process.env.jwtCookieExpirationTime})
+        Cookies.set(process.env.REACT_APP_DARK_MODE_COOKIE, response.data.userInfoResponseDTO.darkMode, {expires: jwtCookieExpirationTime})
         if(Cookies.get(process.env.REACT_APP_ACTIVE_ROLE_COOKIE_NAME) == null) {
             Cookies.set(process.env.REACT_APP_ACTIVE_ROLE_COOKIE_NAME, findDefaultRole(response.data.roles), {expires: jwtCookieExpirationTime});
         }
