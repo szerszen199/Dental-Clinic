4590efa9ef5b4e7f9d999c28e93c3a99f7cfef81
Bartłomiej Graczyk <224301@edu.p.lodz.pl>
2021-05-25 15:05:07 +0200

    #SSBD202101-336 - podłączono formularz rejestracji do implementacji funkcjonalności w rejestracji.

---
+++ frontend/src/components/Registration/RegistrationRequest.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import React from "react";
+
+export function registrationRequest(login, email, password, firstName, lastName, phoneNumber, pesel, language) {
+
+    axios.post(process.env.REACT_APP_BACKEND_URL + "account/create", {
+            login: login,
+            email: email,
+            password: password,
+            firstName: firstName,
+            lastName: lastName,
+            phoneNumber: phoneNumber,
+            pesel: pesel,
+            language: language
+        }
+    )
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+            window.location = "/home";
+        })
+        .catch(function (error) {
+            console.log(JSON.stringify(error));
+        });
+}
