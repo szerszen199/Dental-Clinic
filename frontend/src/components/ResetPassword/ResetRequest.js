fee621d69b3493e35c9f3ba3b9472aa93981d19d
224383 Muszyński Grzegorz <224383@edu.p.lodz.pl>
2021-05-28 00:39:34 +0200

    #SSBD202101-342 dodano: implementacja odświeżania danych w formularzach i listach encji.

--- frontend/src/components/ResetPassword/ResetRequest.js
+++ frontend/src/components/ResetPassword/ResetRequest.js
@@ -3,7 +3,9 @@ import axios from "axios";
 export function makeResetPasswordRequest(login) {
     axios.put(process.env.REACT_APP_BACKEND_URL + "account/reset-password", {
         login: login,
+    }).then((res) => {
+        window.location = "/home";
+    }).catch((res) => {
     })
-    window.location = "/home";
 }
 
