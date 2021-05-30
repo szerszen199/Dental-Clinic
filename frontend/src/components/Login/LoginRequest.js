319955dfe5a8dc6f0645cd494b5f9123b53a28d1
224459 Woźniak Tomasz <224459@edu.p.lodz.pl>
2021-05-30 21:01:46 +0200

    Zaimplementowano wymuszenie zmiany hasła podczas pierwszego logowania użytkownika #SSBD202101-232

--- frontend/src/components/Login/LoginRequest.js
+++ frontend/src/components/Login/LoginRequest.js
@@ -5,31 +5,31 @@ import {jwtCookieExpirationTime} from "../../views/MainView/MainView";
 import "../Alerts/ErrorAlerts/ErrorAlerts";
 import errorAlerts from "../Alerts/ErrorAlerts/ErrorAlerts";
 
-// TODO usuwanie tego gdy minie określony czas czytaj przedawni się
-
 export function makeLoginRequest(login, password, t) {
     axios.post(process.env.REACT_APP_BACKEND_URL + "auth/login", {
         username: login,
         password: password
     }).then((response) => {
-        console.log(response)
-        // TODO: Czas expieracji.
-        Cookies.set(process.env.REACT_APP_JWT_TOKEN_COOKIE_NAME, response.data.authJwtToken.token, {expires: jwtCookieExpirationTime});
-        Cookies.set(process.env.REACT_APP_ROLES_COOKIE_NAME, response.data.roles, {expires: jwtCookieExpirationTime});
-        Cookies.set(process.env.REACT_APP_LOGIN_COOKIE, response.data.username, {expires: jwtCookieExpirationTime})
-        Cookies.set(process.env.REACT_APP_LANGUAGE_COOKIE, response.data.userInfoResponseDTO.language, {expires: jwtCookieExpirationTime})
-        Cookies.set(process.env.REACT_APP_DARK_MODE_COOKIE, response.data.userInfoResponseDTO.darkMode, {expires: jwtCookieExpirationTime})
-        if (Cookies.get(process.env.REACT_APP_ACTIVE_ROLE_COOKIE_NAME) == null) {
-            Cookies.set(process.env.REACT_APP_ACTIVE_ROLE_COOKIE_NAME, findDefaultRole(response.data.roles), {expires: jwtCookieExpirationTime});
+        console.log(response.status)
+        if(response.status === 210){
+            console.log(response)
+            window.location = "/new-password/" + response.data.token;
+        } else {
+            console.log(response)
+            Cookies.set(process.env.REACT_APP_JWT_TOKEN_COOKIE_NAME, response.data.authJwtToken.token, {expires: jwtCookieExpirationTime});
+            Cookies.set(process.env.REACT_APP_ROLES_COOKIE_NAME, response.data.roles, {expires: jwtCookieExpirationTime});
+            Cookies.set(process.env.REACT_APP_LOGIN_COOKIE, response.data.username, {expires: jwtCookieExpirationTime})
+            Cookies.set(process.env.REACT_APP_LANGUAGE_COOKIE, response.data.userInfoResponseDTO.language, {expires: jwtCookieExpirationTime})
+            Cookies.set(process.env.REACT_APP_DARK_MODE_COOKIE, response.data.userInfoResponseDTO.darkMode, {expires: jwtCookieExpirationTime})
+            if (Cookies.get(process.env.REACT_APP_ACTIVE_ROLE_COOKIE_NAME) == null) {
+                Cookies.set(process.env.REACT_APP_ACTIVE_ROLE_COOKIE_NAME, findDefaultRole(response.data.roles), {expires: jwtCookieExpirationTime});
+            }
+            localStorage.setItem(process.env.REACT_APP_JWT_REFRESH_TOKEN_STORAGE_NAME, response.data.refreshJwtToken.token);
+            window.location = "/home";
         }
-        localStorage.setItem(process.env.REACT_APP_JWT_REFRESH_TOKEN_STORAGE_NAME, response.data.refreshJwtToken.token);
-        // TODO: To redirect po poprawnym zalogowaniu, nie podoba mi się, nie korzysta z routera ale inaczej mi nie chce narazie pojsc.
-        window.location = "/home";
     }).catch((response) => {
         if (response.response) {
             errorAlerts(t(response.response.data.message), response.response.status.toString(10));
         }
-
-        // todo Wyświetlić odpowiedni komunikat
     })
 }
