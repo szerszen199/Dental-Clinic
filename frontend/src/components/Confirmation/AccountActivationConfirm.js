446f74687e431e8edb8eb2919bae4b8ee3adae75
224406 Witold Pietrzak <224406@edu.p.lodz.pl>
2021-05-31 01:13:13 +0200

    Zaimplementowano komponenty do frontendowego wyswietlania linkow do potwierdzenia #SSBD202101-325

---
+++ frontend/src/components/Confirmation/AccountActivationConfirm.js
@@ -0,0 +1,43 @@
+import React, {Fragment} from "react";
+import axios from "axios";
+import {withTranslation} from "react-i18next";
+import errorAlertsWithRedirect from "../Alerts/ErrorAlerts/ErrorAlertsWithRedirect";
+import successAlertsWithRedirect from "../Alerts/SuccessAlerts/SuccessAlertsWithRedirect";
+
+class AccountActivationConfirm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.token = this.props.match.params.token;
+        this.state = {}
+        this.t = this.props.t;
+
+    }
+
+
+    componentDidMount() {
+        const {t} = this.props;
+        axios.put(process.env.REACT_APP_BACKEND_URL + "account/confirm", {
+            confirmToken: this.token
+        }).then(response => {
+            this.setState({
+                message: response.data.message
+            })
+            successAlertsWithRedirect(this.t(response.data.message), response.status.toString(), "/home");
+        }).catch(reason => {
+            errorAlertsWithRedirect(t(reason.response.data.message), reason.response.status.toString(), "/home");
+
+        })
+    }
+
+
+    render() {
+        return <Fragment>
+            <div>
+            </div>
+        </Fragment>
+    }
+
+}
+
+export default withTranslation()(AccountActivationConfirm);
