81c753194b7ad02d3926dc2b737ade4976ca91a2
224459 Woźniak Tomasz <224459@edu.p.lodz.pl>
2021-05-30 21:57:17 +0200

    Naprawiono zmianę hasła użytkownika przez administratora i utworzono inny szablon maila związany z tą czynnością #SSBD202101-361

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mok/ejb/managers/AccountManagerImplementation.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mok/ejb/managers/AccountManagerImplementation.java
@@ -510,7 +510,6 @@ public class AccountManagerImplementation extends AbstractManager implements Acc
         } catch (Exception e) {
             throw AccountException.noSuchAccount(e);
         }
-        // TODO: 21.05.2021 Dlugosc do zmiennej w pliku konfiguracyjnym
         String pass = passwordGenerator.generate(32);
         account.setPassword(hashGenerator.generateHash(pass));
         try {
@@ -518,7 +517,6 @@ public class AccountManagerImplementation extends AbstractManager implements Acc
         } catch (Exception e) {
             throw PasswordException.passwordResetFailed();
         }
-        mailProvider.sendGeneratedPasswordMail(account.getEmail(), pass, account.getLanguage());
     }
 
     @Override
@@ -538,7 +536,25 @@ public class AccountManagerImplementation extends AbstractManager implements Acc
         } catch (MailSendingException mailSendingException) {
             throw MailSendingException.editAccountMail();
         }
-        // TODO: send mail with new password
+    }
+
+    @Override
+    public void sendResetPasswordByAdminConfirmationEmail(String login) throws AppBaseException {
+        Account account;
+        try {
+            account = accountFacade.findByLogin(login);
+        } catch (Exception e) {
+            throw AccountException.noSuchAccount(e);
+        }
+        try {
+            mailProvider.sendResetPassByAdminConfirmationMail(
+                    account.getEmail(),
+                    jwtResetPasswordConfirmation.generateJwtTokenForUsername(
+                            login), account.getLanguage()
+            );
+        } catch (MailSendingException mailSendingException) {
+            throw MailSendingException.editAccountMail();
+        }
     }
 
     @Override
