9b2f69f293ccbf2072f6eb63c3f71fbbfb2e1135
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2021-05-31 04:04:22 +0200

    Dodanie java doc dla polowy entities

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/auth/CorsFilter.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/auth/CorsFilter.java
@@ -6,6 +6,9 @@ import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 
+/**
+ * Typ Cors filter dodający odpowiednie nagłówki dla CORSa.
+ */
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
     @Override
