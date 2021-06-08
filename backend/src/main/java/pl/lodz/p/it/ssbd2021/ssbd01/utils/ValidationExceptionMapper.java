2f6578f85b0a3cdf03e4a35a0c17bdf03afa510a
224466 Zdrzalik Przemysław <224466@edu.p.lodz.pl>
2021-06-08 20:30:28 +0200

    poprawiono blad przy bean validation #SSBD202101-415

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/utils/ValidationExceptionMapper.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/utils/ValidationExceptionMapper.java
@@ -1,12 +1,17 @@
 package pl.lodz.p.it.ssbd2021.ssbd01.utils;
 
 
+import pl.lodz.p.it.ssbd2021.ssbd01.common.I18n;
 import pl.lodz.p.it.ssbd2021.ssbd01.mok.dto.response.MessageResponseDto;
 
 import javax.validation.ValidationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Klasa mapowania wyjątków walidacji.
@@ -16,6 +21,11 @@ public class ValidationExceptionMapper implements ExceptionMapper<ValidationExce
 
     @Override
     public Response toResponse(ValidationException exception) {
-        return Response.status(Response.Status.BAD_REQUEST).entity(new MessageResponseDto(exception.getMessage())).build();
+        var message = exception.getMessage();
+        var arg0 = Arrays.stream(message.strip().split(":|,")).filter(s -> !s.contains("arg0")).findFirst();
+        if (arg0.isPresent()) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(new MessageResponseDto(arg0.get())).build();
+        }
+        return Response.status(Response.Status.BAD_REQUEST).entity(new MessageResponseDto(I18n.BEAN_VALIDATION_ERROR)).build();
     }
 }
