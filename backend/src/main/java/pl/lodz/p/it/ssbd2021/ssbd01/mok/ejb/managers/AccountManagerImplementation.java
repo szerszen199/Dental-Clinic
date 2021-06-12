ec2f88ef303345f1633ef1d6784e42152e87a48e
Bartłomiej Graczyk <224301@edu.p.lodz.pl>
2021-06-12 18:31:21 +0200

    #SSBD202101-393 - stworzono encje reprezentującą statystyki ocen lekarza po wizytach.

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mok/ejb/managers/AccountManagerImplementation.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mok/ejb/managers/AccountManagerImplementation.java
@@ -1,5 +1,16 @@
 package pl.lodz.p.it.ssbd2021.ssbd01.mok.ejb.managers;
 
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.annotation.security.PermitAll;
+import javax.ejb.SessionSynchronization;
+import javax.ejb.Stateful;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.inject.Inject;
+import javax.interceptor.Interceptors;
+import javax.servlet.http.HttpServletRequest;
 import pl.lodz.p.it.ssbd2021.ssbd01.common.I18n;
 import pl.lodz.p.it.ssbd2021.ssbd01.entities.AccessLevel;
 import pl.lodz.p.it.ssbd2021.ssbd01.entities.Account;
@@ -30,18 +41,6 @@ import pl.lodz.p.it.ssbd2021.ssbd01.utils.LoggedInAccountUtil;
 import pl.lodz.p.it.ssbd2021.ssbd01.utils.MailProvider;
 import pl.lodz.p.it.ssbd2021.ssbd01.utils.PropertiesLoader;
 import pl.lodz.p.it.ssbd2021.ssbd01.utils.RandomPasswordGenerator;
-
-import javax.annotation.security.PermitAll;
-import javax.ejb.SessionSynchronization;
-import javax.ejb.Stateful;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
-import javax.inject.Inject;
-import javax.interceptor.Interceptors;
-import javax.servlet.http.HttpServletRequest;
-import java.text.ParseException;
-import java.time.LocalDateTime;
-import java.util.List;
 
 
 /**
@@ -84,7 +83,7 @@ public class AccountManagerImplementation extends AbstractManager implements Acc
     private MailProvider mailProvider;
 
     @Inject
-    private JwtResetPasswordConfirmation jwtResetPasswordConfirmation;
+        private JwtResetPasswordConfirmation jwtResetPasswordConfirmation;
     @Inject
     private PropertiesLoader propertiesLoader;
 
@@ -121,7 +120,6 @@ public class AccountManagerImplementation extends AbstractManager implements Acc
                 medicalDocumentation.setMedicationsTaken("");
 
                 medicalDocumentation.setPatient(account);
-
             }
         });
 
