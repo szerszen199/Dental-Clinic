55712fcfe6d05c7c77c32b670904ecca67b2b41f
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2021-06-13 12:16:43 +0200

    Dodanie klasy szyfrującej - #SSBD202101-387

---
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/utils/Encryptor.java
@@ -0,0 +1,35 @@
+package pl.lodz.p.it.ssbd2021.ssbd01.utils;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.inject.Inject;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class Encryptor {
+
+    @Inject
+    private PropertiesLoader propertiesLoader;
+
+    public String encryptMessage(byte[] message) throws NoSuchPaddingException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        byte[] decodedKey = Base64.getDecoder().decode(propertiesLoader.getCipherKey());
+        SecretKey secretKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        return Base64.getEncoder().encodeToString(cipher.doFinal(message));
+    }
+
+    public byte[] decryptMessage(String encryptedMessage) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        byte[] decodedKey = Base64.getDecoder().decode(propertiesLoader.getCipherKey());
+        SecretKey secretKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        return cipher.doFinal(encryptedMessage.getBytes());
+    }
+
+}
