15f798e981624670fa0b8e5c6eb9d5f1a391220c
224248 Antczak Piotr <224248@edu.p.lodz.pl>
2021-06-19 13:44:05 +0200

    dodano edycję wizyty przez recepcjonistę w backendzie #SSBD202101-381

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mow/cdi/endpoints/AppointmentEndpoint.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mow/cdi/endpoints/AppointmentEndpoint.java
@@ -6,17 +6,31 @@ import javax.ejb.EJBTransactionRolledbackException;
 import javax.ejb.Stateful;
 import javax.inject.Inject;
 import javax.interceptor.Interceptors;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+
 import pl.lodz.p.it.ssbd2021.ssbd01.common.I18n;
+import pl.lodz.p.it.ssbd2021.ssbd01.exceptions.mow.AppointmentException;
 import pl.lodz.p.it.ssbd2021.ssbd01.exceptions.mow.DoctorRatingException;
+import pl.lodz.p.it.ssbd2021.ssbd01.mok.dto.response.MessageResponseDto;
+import pl.lodz.p.it.ssbd2021.ssbd01.mow.dto.AppointmentEditRequestDto;
 import pl.lodz.p.it.ssbd2021.ssbd01.mow.dto.response.DoctorAndRateResponseDTO;
 import pl.lodz.p.it.ssbd2021.ssbd01.mow.ejb.managers.AppointmentManager;
+import pl.lodz.p.it.ssbd2021.ssbd01.mow.utils.AppointmentTransactionRepeater;
+import pl.lodz.p.it.ssbd2021.ssbd01.security.EntityIdentitySignerVerifier;
+import pl.lodz.p.it.ssbd2021.ssbd01.security.SignatureFilterBinding;
 import pl.lodz.p.it.ssbd2021.ssbd01.utils.LogInterceptor;
+
+import static pl.lodz.p.it.ssbd2021.ssbd01.common.I18n.DATABASE_OPTIMISTIC_LOCK_ERROR;
 
 /**
  * Typ AppointmentEndpoint - punkt dostępowy dla zapytań związanych z wizytami i lekarzami.
@@ -25,9 +39,15 @@ import pl.lodz.p.it.ssbd2021.ssbd01.utils.LogInterceptor;
 @Stateful
 @Interceptors(LogInterceptor.class)
 public class AppointmentEndpoint {
-    
+
     @Inject
     private AppointmentManager appointmentManager;
+
+    @Inject
+    private EntityIdentitySignerVerifier signer;
+
+    @Inject
+    private AppointmentTransactionRepeater appointmentTransactionRepeater;
 
     /**
      * Pobiera listę lekarz i ich ocen.
@@ -47,5 +67,34 @@ public class AppointmentEndpoint {
         }
         return Response.ok().entity(doctors).build();
     }
-    
+
+    /**
+     * Edytuje wizytę.
+     *
+     * @param appointmentEditRequestDto dane potrzebne do edycji wizyty
+     * @param header                    nagłówek If-Match z podpisem obiektu
+     * @return 400 jezeli się nie powiodło 200 jeżeli się powiodło
+     */
+    @PUT
+    @RolesAllowed({I18n.RECEPTIONIST})
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @SignatureFilterBinding
+    @Path("edit")
+    public Response editAppointment(@NotNull @Valid AppointmentEditRequestDto appointmentEditRequestDto,
+                                    @HeaderParam("If-Match") String header) {
+        if (!signer.verifyEntityIntegrity(header, appointmentEditRequestDto)) {
+            return Response.status(Status.BAD_REQUEST).entity(new MessageResponseDto(DATABASE_OPTIMISTIC_LOCK_ERROR)).build();
+        }
+        try {
+            appointmentTransactionRepeater.repeatTransaction(() -> appointmentManager
+                    .editAppointmentSlot(appointmentEditRequestDto), appointmentManager);
+        } catch (AppointmentException e) {
+            return Response.status(Status.BAD_REQUEST).entity(new MessageResponseDto(e.getMessage())).build();
+        } catch (Exception e) {
+            return Response.status(Status.BAD_REQUEST).entity(new MessageResponseDto(I18n.APPOINTMENT_EDIT_FAILED)).build();
+        }
+        return Response.status(Status.OK).entity(new MessageResponseDto(I18n.APPOINTMENT_EDIT_SUCCESSFUL)).build();
+    }
+
 }
