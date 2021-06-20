ed65d98bd374c2cacce8458e41c563f9b14ad0c5
224406 Witold Pietrzak <224406@edu.p.lodz.pl>
2021-06-20 16:14:03 +0200

    Stworzono DTO dla zwracanych terminow wizyt  #SSBD202101-390

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mow/dto/response/AvailableAppointmentResponseDTO.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mow/dto/response/AvailableAppointmentResponseDTO.java
@@ -1,4 +1,29 @@
 package pl.lodz.p.it.ssbd2021.ssbd01.mow.dto.response;
 
-public class AvaliableAppointmentResponseDTO {
+import pl.lodz.p.it.ssbd2021.ssbd01.entities.Appointment;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+public class AvailableAppointmentResponseDTO {
+
+    @NotNull
+    Long id;
+    @NotNull
+    Long doctorId;
+    @NotNull
+    LocalDateTime date;
+    @NotNull
+    Long version;
+
+
+    public AvailableAppointmentResponseDTO(Appointment appointment) {
+        this.id = appointment.getId();
+        this.doctorId = appointment.getDoctor().getId();
+        this.date = appointment.getAppointmentDate();
+        this.version = appointment.getVersion();
+
+    }
+
+
 }
