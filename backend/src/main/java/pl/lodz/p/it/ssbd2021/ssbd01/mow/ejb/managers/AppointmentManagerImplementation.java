c709c60d974103caec220d0ef710018ddf0739c4
224406 Witold Pietrzak <224406@edu.p.lodz.pl>
2021-06-20 16:15:08 +0200

    Dodano metode zwracajaca terminy wizyt do Managera  #SSBD202101-390

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mow/ejb/managers/AppointmentManagerImplementation.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mow/ejb/managers/AppointmentManagerImplementation.java
@@ -1,18 +1,11 @@
 package pl.lodz.p.it.ssbd2021.ssbd01.mow.ejb.managers;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import javax.annotation.security.PermitAll;
-import javax.ejb.Stateful;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
-import javax.inject.Inject;
-import javax.interceptor.Interceptors;
 import org.apache.commons.lang3.NotImplementedException;
 import pl.lodz.p.it.ssbd2021.ssbd01.entities.Account;
 import pl.lodz.p.it.ssbd2021.ssbd01.entities.Appointment;
 import pl.lodz.p.it.ssbd2021.ssbd01.entities.DoctorRating;
 import pl.lodz.p.it.ssbd2021.ssbd01.exceptions.AppBaseException;
+import pl.lodz.p.it.ssbd2021.ssbd01.exceptions.mow.AppointmentException;
 import pl.lodz.p.it.ssbd2021.ssbd01.exceptions.mow.DoctorRatingException;
 import pl.lodz.p.it.ssbd2021.ssbd01.mow.dto.response.DoctorAndRateResponseDTO;
 import pl.lodz.p.it.ssbd2021.ssbd01.mow.ejb.facades.AccountFacade;
@@ -20,6 +13,15 @@ import pl.lodz.p.it.ssbd2021.ssbd01.mow.ejb.facades.AppointmentFacade;
 import pl.lodz.p.it.ssbd2021.ssbd01.mow.ejb.facades.DoctorRatingFacade;
 import pl.lodz.p.it.ssbd2021.ssbd01.utils.AbstractManager;
 import pl.lodz.p.it.ssbd2021.ssbd01.utils.LogInterceptor;
+
+import javax.annotation.security.PermitAll;
+import javax.ejb.Stateful;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.inject.Inject;
+import javax.interceptor.Interceptors;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Klasa implementująca interfejs menadżera wizyt.
@@ -29,13 +31,13 @@ import pl.lodz.p.it.ssbd2021.ssbd01.utils.LogInterceptor;
 @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
 @Interceptors(LogInterceptor.class)
 public class AppointmentManagerImplementation extends AbstractManager implements AppointmentManager {
-    
+
     @Inject
     private AppointmentFacade appointmentFacade;
-    
+
     @Inject
     private AccountFacade accountFacade;
-    
+
     @Inject
     private DoctorRatingFacade doctorRatingFacade;
 
@@ -80,9 +82,9 @@ public class AppointmentManagerImplementation extends AbstractManager implements
             List<DoctorRating> doctors = doctorRatingFacade.getActiveDoctorsAndRates();
             return doctors
                     .stream()
-                    .map(doctor -> 
-                            new DoctorAndRateResponseDTO(doctor.getDoctor().getFirstName(), 
-                                    doctor.getDoctor().getLastName(), 
+                    .map(doctor ->
+                            new DoctorAndRateResponseDTO(doctor.getDoctor().getFirstName(),
+                                    doctor.getDoctor().getLastName(),
                                     doctor.getAverage()))
                     .collect(Collectors.toList());
         } catch (AppBaseException e) {
@@ -113,5 +115,17 @@ public class AppointmentManagerImplementation extends AbstractManager implements
     @Override
     public List<Account> getAllPatients() {
         throw new NotImplementedException();
+    }
+
+    @Override
+    public List<Appointment> getAppointmentsForReceptionist() throws AppointmentException {
+        try {
+            List<Appointment> appointments = appointmentFacade.findFutureUnassignedAppointments();
+            return appointments;
+        } catch (AppBaseException e) {
+            throw AppointmentException.getAllAppointmentsException();
+        }
+
+
     }
 }
