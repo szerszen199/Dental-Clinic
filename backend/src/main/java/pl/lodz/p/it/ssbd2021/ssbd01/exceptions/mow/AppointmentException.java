d7ce0431d82ca419549e9f691271f791b76750a0
224406 Witold Pietrzak <224406@edu.p.lodz.pl>
2021-06-20 16:11:49 +0200

    Utworzono nowy wyjatek #SSBD202101-390

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/exceptions/mow/AppointmentException.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/exceptions/mow/AppointmentException.java
@@ -1,2 +1,18 @@
-package pl.lodz.p.it.ssbd2021.ssbd01.exceptions.mow;public class AppointmentException {
+package pl.lodz.p.it.ssbd2021.ssbd01.exceptions.mow;
+
+import pl.lodz.p.it.ssbd2021.ssbd01.common.I18n;
+import pl.lodz.p.it.ssbd2021.ssbd01.exceptions.AppBaseException;
+
+public class AppointmentException extends AppBaseException {
+    private AppointmentException(String message) {
+        super(message);
+    }
+
+    private AppointmentException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public static AppointmentException getAllAppointmentsException(){
+        return new AppointmentException(I18n.GET_ALL_APPOINTMENTS_FAILED);
+    };
 }
