93607aaa6b6ff02a2c2e820917008a2d0129dee9
224406 Witold Pietrzak <224406@edu.p.lodz.pl>
2021-06-22 15:53:26 +0200

    Dodano metode do wyswietlania maila z linkiem do oceny wizyty do mailProvidera  #SSBD202101-391

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/utils/MailProvider.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/utils/MailProvider.java
@@ -432,6 +432,21 @@ public class MailProvider {
         }
     }
 
+    public void sendAppointmentRateMail(String email, String lang, String token, Long id) throws MailSendingException {
+        Locale locale = new Locale(lang);
+        ResourceBundle langBundle = ResourceBundle.getBundle("LangResource", locale);
+        String subject = langBundle.getString(I18n.APPOINTMENT_RATE_SUBJECT);
+        String activationLink = buildRatingLink(getContextPath(), token, id);
+        String messageText = paragraph(langBundle.getString(I18n.APPOINTMENT_RATE_TEXT))
+                + hyperlink(activationLink, langBundle.getString(I18n.APPOINTMENT_RATE_LINK));
+        try {
+            mailManager.sendMail(email, subject, getFrom(), messageText, session);
+        } catch (MessagingException e) {
+            throw MailSendingException.mailFailed();
+        }
+    }
+
+
     private String paragraph(String text) {
         return "<p>" + text + "</p>";
     }
@@ -477,6 +492,12 @@ public class MailProvider {
         sb.append("/new-password-admin/");
         sb.append(token);
 
+        return sb.toString();
+    }
+
+    private String buildRatingLink(String contextPath, String token, Long id) {
+        StringBuilder sb = new StringBuilder(getFrontendUrl());
+        sb.append("/rate-appointment/").append(id.toString()).append("/").append(token);
         return sb.toString();
     }
 
