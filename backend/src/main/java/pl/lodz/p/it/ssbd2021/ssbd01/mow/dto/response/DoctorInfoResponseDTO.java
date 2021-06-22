47433281287a70eaf292d73fccde1be2157d6c25
224406 Witold Pietrzak <224406@edu.p.lodz.pl>
2021-06-22 15:24:20 +0200

    Dodano brakujace pliki  #SSBD202101-389

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mow/dto/response/DoctorInfoResponseDTO.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mow/dto/response/DoctorInfoResponseDTO.java
@@ -1,2 +1,44 @@
-package pl.lodz.p.it.ssbd2021.ssbd01.mow.dto.response;public class DoctorInfoResponseDTO {
+package pl.lodz.p.it.ssbd2021.ssbd01.mow.dto.response;
+
+import pl.lodz.p.it.ssbd2021.ssbd01.entities.Account;
+
+import javax.validation.constraints.NotNull;
+
+public class DoctorInfoResponseDTO {
+    @NotNull
+    Long id;
+    @NotNull
+    String firstName;
+    @NotNull
+    String lastName;
+
+    public DoctorInfoResponseDTO(Account account) {
+        this.id = account.getId();
+        this.firstName = account.getFirstName();
+        this.lastName = account.getLastName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
