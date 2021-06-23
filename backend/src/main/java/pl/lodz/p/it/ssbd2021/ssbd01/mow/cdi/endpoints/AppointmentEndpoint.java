c3f9b927961738a97848a950fd0424c7cb37e61b
224248 Antczak Piotr <224248@edu.p.lodz.pl>
2021-06-23 13:30:08 +0200

    poprawiono błędnie złączony PR

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mow/cdi/endpoints/AppointmentEndpoint.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/mow/cdi/endpoints/AppointmentEndpoint.java
@@ -52,28 +52,6 @@ import java.util.stream.Collectors;
 
 import static pl.lodz.p.it.ssbd2021.ssbd01.common.I18n.APPOINTMENT_GET_INFO_FAILED;
 import static pl.lodz.p.it.ssbd2021.ssbd01.common.I18n.APPOINTMENT_NOT_FOUND;
-import javax.annotation.security.DenyAll;
-import javax.annotation.security.RolesAllowed;
-import javax.ejb.EJBTransactionRolledbackException;
-import javax.ejb.Stateful;
-import javax.inject.Inject;
-import javax.interceptor.Interceptors;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import static pl.lodz.p.it.ssbd2021.ssbd01.common.I18n.DATABASE_OPTIMISTIC_LOCK_ERROR;
 
 /**
