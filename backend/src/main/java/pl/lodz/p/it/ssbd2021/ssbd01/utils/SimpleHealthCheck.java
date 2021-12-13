5dd9b1edd8acdab791b5ad5bff09702a1a8c9ea1
Piotr Antczak <66703794+pantczak@users.noreply.github.com>
2021-12-13 18:19:19 +0100

    test

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/utils/SimpleHealthCheck.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/utils/SimpleHealthCheck.java
@@ -1,16 +1,16 @@
-package pl.lodz.p.it.ssbd2021.ssbd01.utils;
+// package pl.lodz.p.it.ssbd2021.ssbd01.utils;
 
-import org.eclipse.microprofile.health.HealthCheck;
-import org.eclipse.microprofile.health.HealthCheckResponse;
-import org.eclipse.microprofile.health.Startup;
+// import org.eclipse.microprofile.health.HealthCheck;
+// import org.eclipse.microprofile.health.HealthCheckResponse;
+// import org.eclipse.microprofile.health.Startup;
 
-import javax.enterprise.context.ApplicationScoped;
+// import javax.enterprise.context.ApplicationScoped;
 
-@Startup
-@ApplicationScoped
-public class SimpleHealthCheck implements HealthCheck {
-    @Override
-    public HealthCheckResponse call() {
-        return HealthCheckResponse.named("Application started").up().build();
-    }
-}
+// @Startup
+// @ApplicationScoped
+// public class SimpleHealthCheck implements HealthCheck {
+//     @Override
+//     public HealthCheckResponse call() {
+//         return HealthCheckResponse.named("Application started").up().build();
+//     }
+// }
