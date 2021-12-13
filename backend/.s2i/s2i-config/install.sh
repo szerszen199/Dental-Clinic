77908d197c58015530e203bc861a57c6f66dc87a
Piotr Antczak <66703794+pantczak@users.noreply.github.com>
2021-12-13 18:11:29 +0100

    FIX

--- backend/.s2i/s2i-config/install.sh
+++ backend/.s2i/s2i-config/install.sh
@@ -4,6 +4,6 @@ source /usr/local/s2i/install-common.sh
 injected_dir=$1
 echo "Running on injected_dir=${injected_dir}"
 
-run_cli_script "${injected_dir}/config.cli"
+run_cli_script "${injected_dir}/setup.cli"
 
 echo "End CLI configuration"
