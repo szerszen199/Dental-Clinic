070736e2d88eded99e3776d000d5bd59432c6dce
Piotr Antczak <66703794+pantczak@users.noreply.github.com>
2021-12-14 01:32:58 +0100

    FIX v2 electric boogaloo

--- backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/utils/ReadinessHealthCheck.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2021/ssbd01/utils/ReadinessHealthCheck.java
@@ -1,42 +1,42 @@
-// package pl.lodz.p.it.ssbd2021.ssbd01.utils;
+package pl.lodz.p.it.ssbd2021.ssbd01.utils;
 
-// import org.eclipse.microprofile.config.inject.ConfigProperty;
-// import org.eclipse.microprofile.health.HealthCheck;
-// import org.eclipse.microprofile.health.HealthCheckResponse;
-// import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
-// import org.eclipse.microprofile.health.Readiness;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
+import org.eclipse.microprofile.health.Readiness;
 
-// import javax.enterprise.context.ApplicationScoped;
-// import javax.inject.Inject;
-// import java.io.IOException;
-// import java.net.Socket;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.io.IOException;
+import java.net.Socket;
 
 
-// @Readiness
-// @ApplicationScoped
-// public class ReadinessHealthCheck implements HealthCheck {
-//     @Inject
-//     @ConfigProperty(name = "POSTGRESQL_SERVICE_HOST", defaultValue = "localhost")
-//     private String host;
-//     @Inject
-//     @ConfigProperty(name = "POSTGRESQL_SERVICE_PORT", defaultValue = "5432")
-//     private int port;
+@Readiness
+@ApplicationScoped
+public class ReadinessHealthCheck implements HealthCheck {
+    @Inject
+    @ConfigProperty(name = "DB_SERVICE_HOST", defaultValue = "mariadb")
+    private String host;
+    @Inject
+    @ConfigProperty(name = "DB_SERVICE_PORT", defaultValue = "3306")
+    private int port;
 
-//     @Override
-//     public HealthCheckResponse call() {
-//         HealthCheckResponseBuilder responseBuilder = HealthCheckResponse.named("Database connection health check");
-//         try {
-//             pingServer(host, port);
-//             responseBuilder.up();
-//         } catch (Exception e) {
-//             responseBuilder.down()
-//                     .withData("error", e.getMessage());
-//         }
-//         return responseBuilder.build();
-//     }
+    @Override
+    public HealthCheckResponse call() {
+        HealthCheckResponseBuilder responseBuilder = HealthCheckResponse.named("Database connection health check");
+        try {
+            pingServer(host, port);
+            responseBuilder.up();
+        } catch (Exception e) {
+            responseBuilder.down()
+                    .withData("error", e.getMessage());
+        }
+        return responseBuilder.build();
+    }
 
-//     private void pingServer(String dbhost, int port) throws IOException {
-//         Socket socket = new Socket(dbhost, port);
-//         socket.close();
-//     }
-// }
+    private void pingServer(String dbhost, int port) throws IOException {
+        Socket socket = new Socket(dbhost, port);
+        socket.close();
+    }
+}
